b923b45f3504760c63c251d568f1bccbaa5d3a09
ishikawa-kazuhiko <85629860+ishikawa-kazuhiko@users.noreply.github.com>
2021-06-13 12:32:12 +0900

    Initial commit

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const UserCard = (props) => {
+  const { user } = props;
+  return (
+    <div>
+      <img height={160} width={120} src={user.image} alt={user.name} />
+      <p>{user.name}</p>
+      <SDl>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>WEB</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </div>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+  }
+`;
